42a15265edb454d2fe576fbd4c275bf4cf7e4038
Derrick Sobodash <spam.here@derricksobodash.com>
2012-10-22 10:47:44 +0800

    Clean up copyright info.

--- estool.py
+++ estool.py
@@ -2,39 +2,15 @@
 # -*- coding: utf-8 -*-
 
 '''
-Title  : Estool
-Version: 0.08
-Author : Derrick Sobodash <derrick@sobodash.com>
-Web    : https://github.com/sobodash/estool
-License: BSD License
+Estool
 
-Copyright (c) 2008, Derrick Sobodash
-All rights reserved
+A tool that edits data structures in Der Langrisser for the Super Famicom.
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions
-are met:
-
-* Redistributions of source code must retain the above copyright notice,
-  this list of conditions and the following disclaimer.
-* Redistributions in binary form must reproduce the above copyright notice,
-  this list of conditions and the following disclaimer in the documentation
-  and/or other materials provided with the distribution.
-* Neither the name of Derrick Sobodash nor the names of his contributors
-  may be used to endorse or promote products derived from this software
-  without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-POSSIBILITY OF SUCH DAMAGE.
+Version:   0.08
+Author:    Derrick Sobodash <derrick@sobodash.com>
+Copyright: (c) 2008, 2011, 2012 Derrick Sobodash
+Web site:  https://github.com/sobodash/estool/
+License:   BSD License <http://opensource.org/licenses/bsd-license.php>
 '''
 
 '''
@@ -2541,7 +2517,7 @@ if(len(sys.argv) > 1):
 
 # Print the copyright information
 print PROGRAM + " " + str(VERSION)
-print "Copyright (c) 2008 " + AUTHOR
+print "Copyright (c) 2008, 2011, 2012 " + AUTHOR
 
 # Initialization code and loop
 app = wx.PySimpleApp()
